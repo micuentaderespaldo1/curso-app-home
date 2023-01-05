97e21716752acf12e067fedfc87e7e816d4ff1ab
unknown <mgrande@DESKTOP-7LHM25P.capres.presidencia.gob.sv>
2023-01-05 16:08:24 -0600

    laravel 9 curso app

---
+++ app/Http/Controllers/RutasController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RutasController extends Controller
+{
+    //
+    public function index(){
+        return 'Esta es la sección de catalogos';
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+   
+});
+Route::get('/catalogos', function(){
+    return 'Esta es la sección de catalogos';
+});
+Route::get('/catalogos/{name_cat}', function($name_cat){
+    return 'usted ha accesado al catalogo de: <b>'.$name_cat.'</b>';
+
+});
+Route::get('/ofertas', function(){
+    return 'Bienvenido a la sección de ofertas';
+});
+Route::get('/ofertas/{name_ofer}/{categoria?}', function($name_ofer, $categoria=null){
+    if($categoria){
+        return 'Usted esta en el catalogo:'.$name_ofer. ', de la categoria de '. $categoria;
+    }else{
+        return 'Usted esta en el catalogo:'.$name_ofer;
+    }
+
+});
